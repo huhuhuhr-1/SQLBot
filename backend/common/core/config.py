bbdff3e43271b49868a174df4afc5af465b8170b
huhuhuhr-1 <1126264627@qq.com>
2025-08-26 18:56:59 +0800

    fix: 本地调试会报错，修复

--- backend/common/core/config.py
+++ backend/common/core/config.py
@@ -1,5 +1,7 @@
+import pathlib
 import secrets
 from typing import Annotated, Any, Literal
+import os
 
 from pydantic import (
     AnyUrl,
@@ -22,7 +24,7 @@ def parse_cors(v: Any) -> list[str] | str:
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         # Use top level .env file (one level above ./backend/)
-        env_file="../.env",
+        env_file=pathlib.Path(__file__).resolve().parent.parent.parent / ".env",
         env_ignore_empty=True,
         extra="ignore",
     )
