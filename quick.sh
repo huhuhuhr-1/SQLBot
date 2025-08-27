5b38934a9b7a10f23776272acdccf6175f1c0f71
huhuhuhr-1 <1126264627@qq.com>
2025-08-27 09:36:17 +0800

    feature: 新增本地编译 way1：原始编译 way2：覆盖式编译

--- quick.sh
+++ quick.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Get the current date in the format YYYYMMDD
+VERSION=$(date +%Y%m%d)
+
 echo "🚀 快速构建 SQLBot..."
 
 export DOCKER_BUILDKIT=1
-docker build -t zf-sqlbot:v1.0.0.20250825 .
+docker build -t zf-sqlbot:v1.0.0.$VERSION .
 
 echo "✅ 完成！"
