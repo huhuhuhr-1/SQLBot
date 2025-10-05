68fd247ed7e525339f4aa0134b09c688a747717f
huhuhuhr-1 <1126264627@qq.com>
2025-10-05 11:57:45 +0800

    feature: 优化启动

--- start.sh
+++ start.sh
@@ -1,15 +1,9 @@
 SSR_PATH=/opt/sqlbot/g2-ssr
 APP_PATH=/opt/sqlbot/app
 
--ENABLE_LOCAL_PG=${ENABLE_LOCAL_PG:-true}
-
-if [ "$ENABLE_LOCAL_PG" = "true" ] && [ -x /usr/local/bin/docker-entrypoint.sh ]; then
-  /usr/local/bin/docker-entrypoint.sh postgres &
-  sleep 5
-  wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo -e "\033[1;32mPostgreSQL started.\033[0m"
-else
-  echo "Skipping embedded PostgreSQL startup (ENABLE_LOCAL_PG=$ENABLE_LOCAL_PG)"
-fi
+/usr/local/bin/docker-entrypoint.sh postgres &
+sleep 5
+wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo -e "\033[1;32mPostgreSQL started.\033[0m"
 
 nohup node $SSR_PATH/app.js &
 
