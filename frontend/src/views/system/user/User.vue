4d35a731b052d911f6cd40f986ee0764cf68f40f
dataeaseShu <tong.shu@fit2cloud.com>
2025-10-24 11:21:35 +0800

    feat(Smart Question): Add question copy

--- frontend/src/views/system/user/User.vue
+++ frontend/src/views/system/user/User.vue
@@ -517,7 +517,7 @@ const copyText = () => {
       ElMessage.success(t('embedded.copy_successful'))
     })
     .catch(function () {
-      ElMessage.error(t('embedded.copy_successful'))
+      ElMessage.error(t('embedded.copy_failed'))
     })
 }
 
