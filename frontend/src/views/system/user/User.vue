15cecdb9e3e7f2124c3e9a42871f47a51b1ec422
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2025-12-25 17:48:25 +0800

    perf: Optimization of LDAP Form User Filter Prompt Text

--- frontend/src/views/system/user/User.vue
+++ frontend/src/views/system/user/User.vue
@@ -434,7 +434,7 @@ const filterOption = ref<any[]>([
       { id: '2', name: 'OIDC' },
       { id: '3', name: 'LDAP' },
       { id: '4', name: 'OAuth2' },
-      { id: '5', name: 'SAML2' },
+      /* { id: '5', name: 'SAML2' }, */
     ],
     field: 'origins',
     title: t('user.user_source'),
