e791eedd383a6314d3db9147657892ede788df15
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2025-12-30 14:40:21 +0800

    fix: API security weakness

--- backend/apps/datasource/api/table_relation.py
+++ backend/apps/datasource/api/table_relation.py
@@ -6,12 +6,14 @@ from fastapi import APIRouter, Path
 
 from apps.datasource.models.datasource import CoreDatasource
 from apps.swagger.i18n import PLACEHOLDER_PREFIX
+from apps.system.schemas.permission import SqlbotPermission, require_permissions
 from common.core.deps import SessionDep
 
 router = APIRouter(tags=["Table Relation"], prefix="/table_relation")
 
 
 @router.post("/save/{ds_id}", response_model=None, summary=f"{PLACEHOLDER_PREFIX}tr_save")
+@require_permissions(permission=SqlbotPermission(role=['ws_admin'], keyExpression="ds_id", type='ds'))
 async def save_relation(session: SessionDep, relation: List[dict],
                         ds_id: int = Path(..., description=f"{PLACEHOLDER_PREFIX}ds_id")):
     ds = session.get(CoreDatasource, ds_id)
@@ -24,6 +26,7 @@ async def save_relation(session: SessionDep, relation: List[dict],
 
 
 @router.post("/get/{ds_id}", response_model=List, summary=f"{PLACEHOLDER_PREFIX}tr_get")
+@require_permissions(permission=SqlbotPermission(role=['ws_admin'], keyExpression="ds_id", type='ds'))
 async def get_relation(session: SessionDep, ds_id: int = Path(..., description=f"{PLACEHOLDER_PREFIX}ds_id")):
     ds = session.get(CoreDatasource, ds_id)
     if ds:
